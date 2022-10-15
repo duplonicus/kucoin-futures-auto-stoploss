4fabcdab1f918a6946ca27ad2f49d8184752c8c8
CORNER-PC\dup <jake.caza@gmail.com>
2022-10-15 19:52:33 -0400

    Revert "Started work on trailing stops"
    This reverts commit 823c2268510716e90df7224e98f46d61e8e3cca9.

--- surreal_db.py
+++ surreal_db.py
@@ -4,8 +4,8 @@ from surrealdb.clients.http import HTTPClient
 # SurrealDB HTTP Client Object
 client = HTTPClient(
     "http://localhost:8000",
-    namespace="",
-    database="",
+    namespace="kucoin",
+    database="kucoin",
     username="root",
     password="root",
 )
@@ -19,40 +19,40 @@ async def create_all(table, data) -> dict:
 
 async def create_with_id(table, custom_id, data) -> dict:
     """ Create a record with a specified id.
-    This will raise an exception if the record already exists. """    
+    This will raise an exception if the record already exists.  """    
     response = await client.create_one(table, custom_id, data)
     return response
 
 async def select_all(table) -> dict:
-    """ Query a table for all records. """
+    """Query a table for all records."""
     response = await client.select_all(table)
     return response
 
 async def select_one(table, custom_id) -> dict:
-    """ Query a table for a specific record by the record's id. """
+    """Query a table for a specific record by the record's id."""
     response = await client.select_one(table, custom_id)
     return response
 
 async def replace_one(table, custom_id, new_data) -> dict:
-    """ Replace a record with a specified id. """
+    """Replace a record with a specified id."""
     response = await client.replace_one(table, custom_id, new_data)
     return response
 
 async def upsert_one(table, custom_id, partial_new_data) -> dict:
-    """ Patch a record with a specified id. """
+    """Patch a record with a specified id."""
     response = await client.upsert_one(table, custom_id, partial_new_data)
     return response
 
 async def delete_all(table) -> dict:
-    """ Delete all records in a table. """
+    """Delete all records in a table."""
     await client.delete_all(table)
 
 async def delete_one(table, custom_id) -> dict:
-    """ Delete a record with a specified id. """
+    """Delete a record with a specified id."""
     await client.delete_one(table, custom_id)
 
 async def my_query(query) -> dict:
-    """ Execute a custom query. """
+    """Execute a custom query."""
     response = await client.execute(query)
     return response
 
