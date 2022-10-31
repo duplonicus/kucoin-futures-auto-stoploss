bf3cc84e517ec5017e806c9585c30ff489f890af
CORNER-PC\dup <jake.caza@gmail.com>
2022-10-31 19:11:19 -0400

    Add spaces to comments

--- surreal_db.py
+++ surreal_db.py
@@ -13,46 +13,46 @@ client = HTTPClient(
 event_loop = asyncio.get_event_loop()
 
 async def create_all(table, data) -> dict:
-    """Create a table and add data."""
+    """ Create a table and add data. """
     response = await client.create_all(table, data)
     return response
 
 async def create_with_id(table, custom_id, data) -> dict:
     """ Create a record with a specified id.
-    This will raise an exception if the record already exists.  """
+    This will raise an exception if the record already exists. """
     response = await client.create_one(table, custom_id, data)
     return response
 
 async def select_all(table) -> dict:
-    """Query a table for all records."""
+    """ Query a table for all records. """
     response = await client.select_all(table)
     return response
 
 async def select_one(table, custom_id) -> dict:
-    """Query a table for a specific record by the record's id."""
+    """ Query a table for a specific record by the record's id. """
     response = await client.select_one(table, custom_id)
     return response
 
 async def replace_one(table, custom_id, new_data) -> dict:
-    """Replace a record with a specified id."""
+    """ Replace a record with a specified id. """
     response = await client.replace_one(table, custom_id, new_data)
     return response
 
 async def upsert_one(table, custom_id, partial_new_data) -> dict:
-    """Patch a record with a specified id."""
+    """ Patch a record with a specified id. """
     response = await client.upsert_one(table, custom_id, partial_new_data)
     return response
 
 async def delete_all(table) -> dict:
-    """Delete all records in a table."""
+    """ Delete all records in a table. """
     await client.delete_all(table)
 
 async def delete_one(table, custom_id) -> dict:
-    """Delete a record with a specified id."""
+    """ Delete a record with a specified id. """
     await client.delete_one(table, custom_id)
 
 async def my_query(query) -> dict:
-    """Execute a custom query."""
+    """ Execute a custom query. """
     response = await client.execute(query)
     return response
 
