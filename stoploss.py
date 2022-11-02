0b43a8139bb83141e74968d48fd092bd50f5c796
CORNER-PC\dup <jake.caza@gmail.com>
2022-11-02 11:05:15 -0400

    Minor improvements

--- stoploss.py
+++ stoploss.py
@@ -1,5 +1,5 @@
 """
-Kucoin Futures automatic stoploss, trailing-stops, take-profits, and algo-trading
+Kucoin Futures automatic stoploss, trailing-stops, and algo-trading
 """
 from kucoin_futures.client import TradeData, UserData, MarketData
 import time
@@ -19,23 +19,9 @@ api_secret = config['api']['secret']
 api_passphrase = config['api']['passphrase']
 
 # Kucoin REST API Wrapper Client Objects
-td_client = TradeData(key=api_key,
-                secret=api_secret,
-                passphrase=api_passphrase,
-                is_sandbox=False,
-                url='https://api-futures.kucoin.com')
-
-ud_client = UserData(key=api_key,
-                secret=api_secret,
-                passphrase=api_passphrase,
-                is_sandbox=False,
-                url='https://api-futures.kucoin.com')
-
-md_client = MarketData(key=api_key,
-                secret=api_secret,
-                passphrase=api_passphrase,
-                is_sandbox=False,
-                url='https://api-futures.kucoin.com')
+td_client = TradeData(key=api_key, secret=api_secret, passphrase=api_passphrase, is_sandbox=False, url='https://api-futures.kucoin.com')
+ud_client = UserData(key=api_key, secret=api_secret, passphrase=api_passphrase, is_sandbox=False, url='https://api-futures.kucoin.com')
+md_client = MarketData(key=api_key, secret=api_secret, passphrase=api_passphrase, is_sandbox=False, url='https://api-futures.kucoin.com')
 
 # TODO: [KFAS-1] Argument parser
 
@@ -43,30 +29,24 @@ md_client = MarketData(key=api_key,
 # Number of seconds between each loop
 sleep_time = 1.5
 
-# Number of ticks away from liquidation price for stoploss price. Must be integer >= 1.
+# Number of ticks away from liquidation price for stoploss
 ticks_from_liq = 2
 
-# Enable take-profit orders at the profit_target_pcnt
-#take_profit = True
-
-# Unrealized ROE percent target for take-profit order
-# .3 is 30%
-#profit_target_pcnt = 0.30
-
-# The get_start_trailing_pcnt() function returns the break even percent of the trade plus this percentage.
+# The get_start_trailing_pcnt() function returns the break even percent of the trade plus this percentage
 # .1 is 10%
 start_trailing_pcnt_lead = .10 # Example: at 20X with 0.08% fees, break even is at 3.2% ROE, add 10%, start trailing at 13.2% ROE
 
 # The amount of leeway between the start_trailing_pcnt and the trailing stop.
-# .05 is 5%
-leeway_pcnt = .05 # Example: start trailing at 13.2% ROE, trailing stop is placed at 7.2%
+leeway_pcnt = .05 # Example: start trailing at 13.2% unrealised ROE, trailing stop is placed at 7.2%
 
-# How much the unrealised PnL must increase to bump the stop.
-# .05 is 5%
+# How much the unrealised ROE must increase to bump the stop
 trailing_bump_pcnt = .05
 
 # Your trading fee based on VIP level
 fee = 0.08
+
+# The percentage of account balance you normally trade with, not used for any caculations
+trade_pcnt = 0.10
 
 # Set to True after installing SurrealDB: https://surrealdb.com/
 database = True
@@ -85,6 +65,7 @@ symbols_dict = {}
 symbols = []
 stop_symbols = []
 initialized = False
+strftime = '%A %Y-%m-%d, %H:%M:%S'
 
 """ Functions """
 def init() -> None:
@@ -93,7 +74,7 @@ def init() -> None:
     pyfiglet.print_figlet("Kucoin Futures Position Manager", 'alphabet', 'GREEN')
     print("\033[91m{}\033[00m".format('By Duplonicus\n'))
     if database:
-        print(f'> [{datetime.now().strftime("%A %Y-%m-%d, %H:%M:%S")}] Connecting to SurrealDB...')
+        print(f'> [{datetime.now().strftime(strftime)}] Connecting to SurrealDB...')
         try:
             table = event_loop.run_until_complete(select_all("symbol"))
         except Exception as e:
@@ -209,7 +190,7 @@ def get_tick_size(pos: dict) -> str:
 def get_start_trailing_pcnt(pos: dict) -> float:
     """ Returns the break-even percent + leeway_pcnt """
     leverage = get_leverage(pos)
-    start_trailing_pcnt = (fee * leverage) * 2 # This is the percentage that the trade breaks even
+    start_trailing_pcnt = (fee * leverage) * 2 # The percentage that the trade breaks even
     start_trailing_pcnt = start_trailing_pcnt * 1e-2 # Shift the decimal 2 places to the left
     start_trailing_pcnt = start_trailing_pcnt + start_trailing_pcnt_lead # Add some amount to it so that after subtracting
     return start_trailing_pcnt                                           # the leeway_pcnt, the first trailing stop will be in profit
@@ -218,7 +199,7 @@ def cancel_stops_without_pos() -> None:
     """ Cancels stops without a position. """
     for item in stops["items"]:
             if item["symbol"] not in symbols:
-                print(f'> [{datetime.now().strftime("%A %Y-%m-%d, %H:%M:%S")}] No position for {item["symbol"]}! CANCELLING STOP orders...        ')
+                print(f'> [{datetime.now().strftime(strftime)}] No position for {item["symbol"]}! CANCELLING STOP orders...        ')
                 td_client.cancel_all_stop_order(item["symbol"])
 
 def round_to_tick_size(number: float | int, tick_size: float | int | str) -> float:
@@ -285,7 +266,7 @@ def add_far_stop(pos: dict) -> None:
     side = 'buy' if direction == 'short' else 'sell'
     # Submit the stoploss order
     oId = f'{pos["symbol"]}far'
-    print(f'> [{datetime.now().strftime("%A %Y-%m-%d, %H:%M:%S")}] Submitting STOP order for {pos["symbol"]} {leverage} X {direction} position: {pos["currentQty"]} contracts @ {stop_price}')
+    print(f'> [{datetime.now().strftime(strftime)}] Submitting STOP order for {pos["symbol"]} {leverage} X {direction} position: {pos["currentQty"]} contracts @ {stop_price}')
     # 'size' and 'lever' can be 0 because 'stop' has a value. closeOrder=True ensures a position won't be entered or increase. 'MP' means mark price, 'TP' means last traded price, 'IP' means index price
     td_client.create_limit_order(clientOid=oId, closeOrder=True, type='market', side=side, symbol=pos['symbol'], stop=stop, stopPrice=stop_price, stopPriceType='MP', price=0, lever=0, size=pos["currentQty"])
 
@@ -330,7 +311,7 @@ def add_trailing_stop(pos: dict) -> None:
     trail_price = get_trailing_stop_price(pos)
     amount = pos['currentQty']
     oid = f'{pos["symbol"]}trail'
-    print(f'> [{datetime.now().strftime("%A %Y-%m-%d, %H:%M:%S")}] Submitting TRAILING STOP order for {pos["symbol"]} {leverage} X {direction} position: {pos["currentQty"]} contracts @ {trail_price}')
+    print(f'> [{datetime.now().strftime(strftime)}] Submitting TRAILING STOP order for {pos["symbol"]} {leverage} X {direction} position: {pos["currentQty"]} contracts @ {trail_price}')
     # Size and lever can be 0 because stop has a value. closeOrder=True ensures a position won't be entered or increase. 'MP' means mark price, 'TP' means last traded price, 'IP' means index price
     td_client.create_limit_order(clientOid=oid, closeOrder=True, type='limit', side=side, symbol=pos['symbol'], stop=stop, stopPrice=trail_price, stopPriceType='MP', price=0, lever=0, size=amount)
 
@@ -348,9 +329,9 @@ def main():
 
             if not initialized:
                 init()
-                print(f'> [{datetime.now().strftime("%A %d-%m-%Y, %H:%M:%S")}] Stops will begin trailing dynamically depending on leverage with a leeway of {leeway_pcnt * 100}%')
-                balance = get_futures_balance()
-                print(f'Account Balance: {balance} USDT, 10% of Account Balance: {balance * 1e-1}')
+                print(f'> [{datetime.now().strftime(strftime)}] Stops will begin trailing dynamically depending on leverage with a leeway of {leeway_pcnt * 100}%')
+                balance = round(get_futures_balance(), 2)
+                print(f'> [{datetime.now().strftime(strftime)}] Account Balance: {balance} USDT -> {round(trade_pcnt * 1e2)}% of Account Balance: {round(balance * trade_pcnt, 2)} USDT')
 
             get_positions()
             get_stops()
@@ -361,7 +342,7 @@ def main():
                 cancel_stops_without_pos()
 
             if not positions:
-                print(f"> [{datetime.now().strftime('%A %d-%m-%Y, %H:%M:%S')}] No active positions... Start a trade!                        ", end="\r")
+                print(f"> [{datetime.now().strftime(strftime)}] No active positions... Start a trade!                              ", end="\r")
                 time.sleep(sleep_time)
                 continue
 
@@ -379,22 +360,27 @@ def main():
                 # This doesn't work when there are multiple positions
                 # The extra spaces are to make sure there is no remaining text after using end='\r'
                 if len(symbols) == 1:
-                    print(f'> [{datetime.now().strftime("%A %Y-%m-%d, %H:%M:%S")}] Active positions:',
+                    print(f'> [{datetime.now().strftime(strftime)}] Active positions:',
                         ''.join(str(get_leverage(pos)).upper() for pos in positions), 'X', ' '.join(str(pos['symbol']) for pos in positions),
                         """ ''.join(str(pos['direction']).upper() for pos in pos_data.values()), """ ' '.join(str(pos['markPrice']) for pos in positions), '$',
                         ''.join(str(pos['currentQty']) for pos in positions), '@', ''.join(str(round(pos['unrealisedRoePcnt'] * 100, 2)) for pos in positions),
                         '%                                              ', end='\r') # the part that's commented out can't be done without the old pos_data dict
                 else:
-                    print(f'> [{datetime.now().strftime("%A %Y-%m-%d, %H:%M:%S")}] Active positions: {symbols}                           ', end='\r')
+                    print(f'> [{datetime.now().strftime(strftime)}] Active positions: {symbols}                           ', end='\r')
 
             time.sleep(sleep_time)
 
         except KeyboardInterrupt:
             # TODO: [KFAS-18] Don't say 'nice trades' if the PnL for the session is negative
-            cancel_stops_without_pos()
-            session = event_loop.run_until_complete(select_all('session'))
+            if stops is not None:
+                cancel_stops_without_pos()
+            end_balance = get_futures_balance()
+            session_pnl = round(end_balance - balance, 2)
             quote = requests.get("https://zenquotes.io/api/random").json()[0]["q"]
-            print('\n', quote, "Nice trades!!! See you tomorrow... :)                                           ")
+            if session_pnl >= 0:
+                print('\n', quote, "Nice trades! See you tomorrow... :)                                             ")
+            else:
+                print('\n', quote, "Those sure were some trades... See you tomorrow... :)                           ")
             quit()
 
         except Exception as e:
